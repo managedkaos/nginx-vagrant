854ba172cbbc57273c2c8309bfa0f2a68d485802
Michael Jenkins <michael.jenkins@disney.com>
2018-04-22 00:12:09 -0700

    Add perms

--- Ubuntu/provision.sh
+++ Ubuntu/provision.sh
@@ -2,5 +2,6 @@ apt update
 apt install -y nginx elinks php-fpm php-mysql mariadb-server mariadb-client
 cp /home/vagrant/default.conf /etc/nginx/sites-available/default
 cp /home/vagrant/info.php /var/www/html
+chmod +r /var/www/html/info.php
 systemctl restart nginx
 systemctl status nginx --no-pager
