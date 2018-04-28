d48454926c91c788229ea061c71bb89d5f7678de
Michael Jenkins <michael.jenkins@disney.com>
2018-04-28 13:34:35 -0700

    add notes and html

--- Ubuntu/info.php
+++ Ubuntu/info.php
@@ -1,4 +1,11 @@
-<?php
-phpinfo(); // Show all information, defaults to INFO_ALL
-phpinfo(INFO_MODULES); // Show just the module information.
-?>
+<html>
+	<head>
+		<title>PHP Info</title>
+	</head>
+	<body>
+		<?php
+			phpinfo(); // Show all information, defaults to INFO_ALL
+			phpinfo(INFO_MODULES); // Show just the module information.
+		?>
+	</body>
+</html>
---
+++ notes.md
@@ -0,0 +1,14 @@
+# Key Files and Directories
+
+## Directories
+
+* /etc/nginx
+* /etc/nginx/conf.d
+* /etc/nginx/sites-available
+* /etc/nginx/sites-enabled
+* /var/log/nginx
+* /var/www/html
+
+## Files
+* /etc/nginx/nginx.conf
+* /etc/nginx/sites-available/default
